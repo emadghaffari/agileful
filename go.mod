688f19f7a64f11e95224a52cb1c81ece1ee3d529
Emad <emadghaffariii@gmail.com>
2021-09-30 08:33:08 +0000

    cleanup

--- go.mod
+++ go.mod
@@ -11,4 +11,5 @@ require (
 	github.com/stretchr/testify v1.7.0
 	github.com/valyala/fasthttp v1.30.0 // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber"
 
 	"github.com/emadghaffari/agileful/app"
@@ -10,10 +8,5 @@ import (
 
 func main() {
 	fbr := fiber.New()
-
 	app.Base.StartApplication(fbr)
-
-	fbr.Shutdown()
-
-	log.Fatal(fbr.Listen(":3000"))
 }
